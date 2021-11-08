6c8b085f7247f5bd08c2552f3812bd075ebac7b2
Md Aliff <md.aliff190@gmail.com>
2021-11-08 17:43:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,21 +13,19 @@ const auth = require("./routes/api/auth");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 
-// Serve static  asets in production
-if(process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    })
-}
-
-
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/posts", posts);
 
+// Serve static  asets in production
+if(process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
