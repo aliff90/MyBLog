07f3151d80a07e39452735b9aa1eb1188657a861
Md Aliff <md.aliff190@gmail.com>
2021-11-08 11:29:16 +0800

    Preparing for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const connectDB = require("./config/db");;
+const connectDB = require("./config/db");
+const path = require("path");
 
 // Connect Databse
 connectDB();
@@ -11,6 +12,16 @@ app.use(express.json());
 const auth = require("./routes/api/auth");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
+
+// Serve static  asets in production
+if(process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 
 app.use("/api/users", users);
